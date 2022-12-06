4c2eddf91218ee274706ad6cfba4d21513498447
peder <pglandsverk@gmail.com>
2022-12-06 20:20:52 +0100

    Solved day 6. Bit confusing?

---
+++ 2022/6/script.hs
@@ -0,0 +1,29 @@
+marker mem cond seq = marker' mem init_mem init_seq 
+   where
+      marker' :: Int -> [Char] -> [Char] -> Int
+      marker' acc prev [] = -1 
+      marker' acc prev xs 
+         | (cond (prev ++ (take 1 xs))) = acc
+         | otherwise = marker' (acc+1) ((tail prev) ++ [head xs]) (tail xs)
+
+      (init_mem, init_seq) = splitAt mem seq
+
+unique seq = unique' [] seq
+   where
+      unique' seen [] = seen
+      unique' seen seq' = unique' seen' xs
+         where
+            (x,xs) = splitAt 1 seq'
+            seen' 
+               | not $ (x `elem` seen) = seen ++ [x]
+               | otherwise = seen
+
+all_unique xs = (length (unique xs)) == (length xs)
+
+main = do
+   input <- getContents
+   let start_of_packet = marker 4 all_unique
+   let start_of_message = (\x -> (marker 13 all_unique x) + 1) -- ??
+
+   print (start_of_packet input)
+   print (start_of_message input)
